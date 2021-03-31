905953c1844fc544527c0b902c764b6f8165685b
timevortex <52909529+timevoltex@users.noreply.github.com>
2021-03-31 14:07:37 +0900

    영화 api 연결, 메인 이미지(배너)를 구현

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,8 @@
 //SERVER ROUTES
-export const USER_SERVER = '/api/users';
+export const USER_SERVER = "/api/users";
+
+export const API_URL = "https://api.themoviedb.org/3/";
+
+export const IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
+
+export const API_KEY = "d4d34032b5d96237b46972c1842c948f";
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,16 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import { FaCode } from "react-icons/fa";
+import { API_KEY, API_URL, IMAGE_BASE_URL } from "../../Config";
+import MainImage from "./Section/MainImage";
 
 function LandingPage() {
-    return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
-            </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
-    )
+  const [movies, setMovies] = useState([]);
+  const [mainMovie, setMainMovie] = useState(null);
+  useEffect(() => {
+    const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=ko&page=1`;
+    fetch(endpoint)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        setMovies([...response.results]);
+        setMainMovie(response.results[0]);
+      });
+  }, []);
+  return (
+    <div style={{ width: "100%", margin: 0 }}>
+      {mainMovie && (
+        <MainImage
+          image={`${IMAGE_BASE_URL}w1280${mainMovie.backdrop_path}`}
+          title={`${mainMovie.original_title}`}
+          description={mainMovie.overview}
+        />
+      )}
+      <div style={{ width: "85%", margin: "1rem auto" }}>
+        <h2>Movies by latest</h2>
+        <hr />
+        {/* Movie Grid Cards */}
+      </div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <button>Load More</button>
+      </div>
+    </div>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
